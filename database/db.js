d254231c9985caa9ab609f075a366a16451f5383
zacbs <zacbs14@gmail.com>
2023-08-25 17:40:21 +1000

    refactored db.js

--- database/db.js
+++ database/db.js
@@ -3,14 +3,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-async function dbOpen() {
-  await mongoose.connect(process.env.ATLAS_DB_URL)
-  console.log('Database connected')
-}
-
 async function dbClose() {
   await mongoose.connection.close()
   console.log('Database disconnected')
 }
 
-export { dbOpen, dbClose }
+await mongoose.connect(process.env.ATLAS_DB_URL)
+console.log('Database connected')
+
+export { dbClose }

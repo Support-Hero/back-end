1a0f097f37785b1ead17ebfb3078fc125e0131f9
zacbs <zacbs14@gmail.com>
2023-08-26 14:15:55 +1000

    finished seed data

--- seed.js
+++ seed.js
@@ -104,27 +104,109 @@ const fakeClients = [
         followUp: false,
       }],
       assignedWorkers: threeUsers
+  },
+  {
+    firstName: 'Paul',
+    lastName: 'Smith',
+    address: '123 Fake Ave',
+    phoneNumber: '0455555555',
+    clientNotes: [
+      {
+        date: Date(),
+        goals: 'lorem Ipsum dolor sit',
+        presentation: 'lorem Ipsum dolor sit',
+        actions: 'lorem Ipsum dolor sit',
+        outcome: 'lorem Ipsum dolor sit et',
+        followUp: true,
+        followUpNote: 'lorem Ipsum Lore'
+      },
+      {
+        date: Date(),
+        goals: 'lorem Ipsum',
+        presentation: 'lorem Ipsum',
+        actions: 'lorem Ipsum',
+        outcome: 'lorem Ipsum',
+        followUp: false,
+      }],
+      assignedWorkers: threeUsers
+  },
+  {
+    firstName: 'Paul',
+    lastName: 'Smith',
+    address: '123 Fake Ave',
+    phoneNumber: '0455555555',
+    clientNotes: [
+      {
+        date: Date(),
+        goals: 'lorem Ipsum dolor sit',
+        presentation: 'lorem Ipsum dolor sit',
+        actions: 'lorem Ipsum dolor sit',
+        outcome: 'lorem Ipsum dolor sit et',
+        followUp: true,
+        followUpNote: 'lorem Ipsum Lore'
+      },
+      {
+        date: Date(),
+        goals: 'lorem Ipsum',
+        presentation: 'lorem Ipsum',
+        actions: 'lorem Ipsum',
+        outcome: 'lorem Ipsum',
+        followUp: false,
+      }],
+      assignedWorkers: users[2]
+  },{
+    firstName: 'Paul',
+    lastName: 'Smith',
+    address: '123 Fake Ave',
+    phoneNumber: '0455555555',
+    clientNotes: [
+      {
+        date: Date(),
+        goals: 'lorem Ipsum dolor sit',
+        presentation: 'lorem Ipsum dolor sit',
+        actions: 'lorem Ipsum dolor sit',
+        outcome: 'lorem Ipsum dolor sit et',
+        followUp: true,
+        followUpNote: 'lorem Ipsum Lore'
+      },
+      {
+        date: Date(),
+        goals: 'lorem Ipsum',
+        presentation: 'lorem Ipsum',
+        actions: 'lorem Ipsum',
+        outcome: 'lorem Ipsum',
+        followUp: false,
+      }],
+      assignedWorkers: users[4]
   }
 ]
+
+// Insert fake clients into the database
 await ClientModel.deleteMany()
 console.log('Deleted Clients')
 const clients = await ClientModel.insertMany(fakeClients)
 console.log('Inserted Clients')
 
+// function to update clients list of users
 async function addClientsToUsers (test, user) {
   const updatedEntry = {clients: []}
-  updatedEntry.clients.concat(test._id)
-  console.log(test._id)
+  updatedEntry.clients = test
   await UserModel.findByIdAndUpdate(user, updatedEntry)
 }
 
-const twoClients = [clients[0]._id, clients[1]._id]
+// Create bogus arrays for above function
+const twoClients = [clients[0], clients[1]]
+const threeClients = [clients[2], clients[4], clients[0]]
+const twodifferentClients = [clients[4], clients[3]]
 
-// console.log(twoClients)
+// Call function on each user to update them with random clients
+addClientsToUsers(twoClients, users[0])
+addClientsToUsers(twodifferentClients, users[1])
+addClientsToUsers(threeClients, users[2])
+addClientsToUsers(clients[3], users[3])
+addClientsToUsers(clients[4], users[4])
 
-// addClientsToUsers(twoClients, users[0])
-// addClientsToUsers(clients[1]._id, users[1])
-
+console.log('Adding refences on users collection -> clients collection')
 
 // Close the connection to the database
 setTimeout(dbClose, 3000)

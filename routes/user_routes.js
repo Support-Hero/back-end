06ccfeaeb9051a58ca7ce32767d153481ab67390
zacbs <zacbs14@gmail.com>
2023-08-28 18:54:50 +1000

    PUT request for users

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -36,8 +36,49 @@ router.post('/', async (req, res) => {
   }}
 })
 
+// Maybe a better solution than chaining if statements ?
 // PUT update a user
-// TODO
+router.put('/', async (req, res) => {
+  try {
+    const updatedUser = {}
+    if (req.body.email) {
+      updatedUser.email = req.body.email
+    }
+    if (req.body.password) {
+      updatedUser.password = req.body.password
+    }
+    if (req.body.phoneNumber) {
+      updatedUser.phoneNumber = req.body.phoneNumber
+    }
+    if (req.body.firstName) {
+      updatedUser.firstName = req.body.firstName
+    }
+    if (req.body.lastName) {
+      updatedUser.lastName = req.body.lastName
+    }
+    if (req.body.isManager) {
+      updatedUser.isManager = req.body.isManager
+    }
+    if (req.body.clients) {
+      const clients = await ClientModel.find({ name: req.body.category })
+      if (clients) {
+        updatedUser.clients = clients
+      } else {
+        res.status(400).send({ error: 'Clients not found' })
+      }
+    }
+    const user = await UserModel.findByIdAndUpdate(req.params.id, updatedUser, { new: true })
+    if (entry) {
+      res.send(entry)
+    } else {
+      res.status(404).send({ error: 'User not found' })
+    }
+  }
+  catch (err) {
+      res.status(500).send({ error: err.message })
+    }
+  
+})
 
 // Delete a user
 router.delete('/:id', async (req, res) => {
